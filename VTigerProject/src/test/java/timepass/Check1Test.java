8370787c5ac1238e25538c94109350815fd6f53a
Tanuja N Sonawale <Tanuja N Sonawale@TYSS-Tanuja-N-Sonawale>
2024-10-01 13:25:56 +0530

    Adding one more class to my package

---
+++ VTigerProject/src/test/java/timepass/Check1Test.java
@@ -0,0 +1,10 @@
+package timepass;
+
+import org.testng.annotations.Test;
+
+public class Check1Test {
+	@Test
+	public void show_Test() {
+		System.out.println("show_Test method of Check1Test class");
+	}
+}
